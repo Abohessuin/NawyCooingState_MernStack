80f6bd49cdc1685ceb146779a8d24bc3b112cf23
Mohamed Hessuin <57597278+Abohessuin@users.noreply.github.com>
2021-07-10 20:07:25 +0200

    Add files via upload

--- server_side/index.js
+++ server_side/index.js
@@ -3,6 +3,7 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 import postRoutes from "./routes/post.js";
+
 //variables
 const app = express();
 const CONNECTION_URL =
@@ -14,6 +15,10 @@ app.use(bodyParser.urlencoded({ Limit: "30mb", extended: true }));
 app.use(cors());
 
 app.use("/posts", postRoutes);
+app.get("/",(req,res) => {
+  res.send('hello')
+})
+
 
 // Database and server listening connection setup
 mongoose.set("useFindAndModify", false);
